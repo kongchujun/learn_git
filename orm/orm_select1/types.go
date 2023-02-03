48c9d7fc3742cb4a7613d37de82e8fe11b94249f
chujun <985145157@qq.com>
2023-02-04 00:05:39 +0800

    orm select where: add types file

---
+++ orm/orm_select1/types.go
@@ -0,0 +1,24 @@
+package ormselect1
+
+import (
+	"context"
+	"database/sql"
+)
+
+type Querier[T any] interface {
+	Get(ctx context.Context) (*T, error)
+	GetMuti(ctx context.Context) ([]*T, error)
+}
+
+type Excecutor interface {
+	Exec(ctx context.Context) (sql.Result, error)
+}
+
+type QueryBuilder interface {
+	Build() (Query error)
+}
+
+type Query struct {
+	SQL  string
+	Args []any
+}
